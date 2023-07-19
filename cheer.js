94fdec2dee881dc3acca103f610892b5a356f299
Deadhimo <scientific.5t4r@gmail.com>
2023-07-20 02:40:00 +0900

    ver 1

---
+++ cheer.js
@@ -0,0 +1,13 @@
+const songs = document.getElementById("song1","song2","song3","song4","song5","song6","ct0","ct1","ct2","ct3","ct4","ct5","ct6");
+
+const songsParent = songs.parentNode;
+
+document.addEventListener("DOMContentLoaded",function(){
+    
+    if(songs === ""){
+        forEach.songsParent.removeChild(songs);
+    
+    };
+});
+
+//querySelectorAll と forEach songs.lengthを用いてかきなおす！
